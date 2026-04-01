aa32799cc65b74f00a42cb0b135a8159d8af55cf
Gabriel Cardoso <gabrielvictorcardosopeace@gmail.com>
2026-04-01 12:37:51 -0300

    alteração inicial na main

--- src/App.tsx
+++ src/App.tsx
@@ -1,121 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+// Nome da function sempre em pascal case. Ex: ExemploDeNome
 
-function App() {
-  const [count, setCount] = useState(0)
+export function App(){ // Export nomeado diretamente da função
+    // console.log('oi')
 
-  return (
-    <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
-
-      <div className="ticks"></div>
-
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
-    </>
-  )
+    return(
+        // Os elementos devem estar envoltos por apenas um elemento pai
+        <> 
+            <h1>Olá Mundo!</h1>
+            <br />
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde saepe numquam laudantium minus sunt maxime ipsum officiis cupiditate maiores explicabo beatae obcaecati dicta libero exercitationem ullam, aperiam repellat provident corporis.</p>
+            <p>Quo deleniti similique consectetur id asperiores doloribus maxime rem magnam! Commodi accusamus architecto magnam deserunt temporibus nobis sit iste nihil molestias! Ipsa ad accusantium laboriosam aperiam aut aliquam, maiores excepturi!</p>
+            <p>Aut voluptate enim facere expedita reiciendis, culpa perspiciatis maxime cupiditate aliquam laborum! Beatae, possimus quis! Fuga veritatis sint suscipit omnis laboriosam voluptatibus debitis voluptates pariatur. Officia distinctio placeat facere tenetur?</p>
+            <p>Dolorum maxime deleniti quasi ratione quibusdam necessitatibus maiores. Earum numquam recusandae veritatis molestias neque voluptate. Atque quae soluta vitae similique blanditiis voluptatem, temporibus explicabo, voluptatum unde, totam cupiditate distinctio ex?</p>
+        </>
+    );
 }
 
-export default App
+// export default App - Exort não nomeado
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+
+// import QualquerCoisa from './App' - Export não nomeado, o dev o nomeia e pode alterar o seu nome como quiser, ms pode gerar problemas
+import { App } from './App'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    {/* <QualquerCoisa /> */}
     <App />
   </StrictMode>,
 )
